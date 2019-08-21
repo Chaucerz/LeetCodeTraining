88e8724f05f33a601e119a29b3d252d81f16f0e7
Chaucerz <327475589@qq.com>
2019-08-21 22:17:51 +0800

    2019.8.21第2次提交

---
+++ src/leetcode_23_mergeKLists/ListNode.java
@@ -0,0 +1,15 @@
+package leetcode_23_mergeKLists;
+
+/**
+ * @author Chaucer
+ * @date 2019-08-21 22:16
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+
+}
---
+++ src/leetcode_23_mergeKLists/Solution.java
@@ -0,0 +1,39 @@
+package leetcode_23_mergeKLists;
+
+
+
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author Chaucer
+ * @date 2019-08-21 22:14
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0){
+            return null;
+        }
+
+        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(lists.length, Comparator.comparingInt(value -> value.val));
+        ListNode dummyNode = new ListNode(-1);
+        ListNode curr = dummyNode;
+
+        for (ListNode list : lists) {
+            if(list != null) {
+                priorityQueue.add(list);
+            }
+        }
+
+        while (!priorityQueue.isEmpty()) {
+            ListNode node = priorityQueue.poll();
+            curr.next = node;
+            curr = curr.next;
+            if(curr.next != null) {
+                priorityQueue.add(curr.next);
+            }
+        }
+        return dummyNode.next;
+    }
+}
